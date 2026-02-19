c04d728b82d86882d457aa59e99d125e4bd05827
manikanta Routhu <manikantar129@gmail.com>
2026-02-19 12:38:20 +0530

    commited

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,33 @@
-provider = "aws" {
-    region = "us-east-1"
+# resource <resource-type> <resource-name>
+resource "aws_instance" "db" {
+    ami = "ami-0220d79f3f480ecf5"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    instance_type = "t3.micro"
+    tags ={
+        Name = "db"
+    }
+}
+
+resource "aws_security_group" "allow_ssh" {
+   name = "allow_ssh"
+   description = "allowing ssh access"
+
+   ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   } 
+
+   egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   tags = {
+    Name = "allow_ssh"
+    createdBy = "manikanta"
+   }
 }
