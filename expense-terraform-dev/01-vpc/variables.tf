a6b3ffe98fdb3daaeaf132fa1b65890e87ee6798
manikanta Routhu <manikantar129@gmail.com>
2026-02-26 01:02:17 +0530

    add env

--- expense-terraform-dev/01-vpc/variables.tf
+++ expense-terraform-dev/01-vpc/variables.tf
@@ -3,6 +3,11 @@ variable "project_name" {
     default = "expense"
 }
 
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
 variable "common_tags" {
     default = {
         project = "expense"
